9e1cf59df6ebc36d52ac8fdaad318e27b5f58610
mrzunti <mrzunti@csx1.cs.ucalgary.ca>
2016-02-10 12:59:17 -0700

    Updated GameRules

--- GameRules.hs
+++ GameRules.hs
@@ -4,7 +4,39 @@ module GameRules where
 
 import ApocTools
 
-isValidMove :: GameState -> PlayType -> Player -> IO [(Int,Int)]
-isValidMove g t p = do
-                    putStrLn "Spaget!"
-                    return $ (0,0):[]
+--Custom Types------------------------------------------------------
+data Result = VALID | INVALID | CAPTURE deriving (Eq)
+data WinState = BLACK | WHITE | DRAW | NONE deriving (Eq) 
+
+-- MOVE LEGALITY CHECKERS
+-- Filter move to Normal or PawnPlacement checkers
+isValidMove :: [(Int,Int)] -> [(Int,Int)] -> GameState -> PlayType -> Player -> Result
+isValidMove i o g t p =
+                     if (checkBounds o)
+                        then if (pieceOwned i g p)
+                            then if (t == Normal)
+                            then isValidNormalMove i o g p
+                            else isValidPawnPlacement i g
+                        else INVALID
+                     else INVALID
+
+-- First, check output boundaries
+checkBounds :: [(Int,Int)] -> Bool
+checkBounds _ = True
+
+-- Check if input position corresponds to a player's piece
+pieceOwned :: [(Int,Int)] -> GameState -> Player -> Bool
+pieceOwned _ _ _ = True
+
+
+-- TODO
+isValidNormalMove :: [(Int,Int)] -> [(Int,Int)] -> GameState -> Player -> Result
+isValidNormalMove _ _ _ _ = VALID
+
+-- TODO
+isValidPawnPlacement :: [(Int,Int)] -> GameState -> Result
+isValidPawnPlacement _ _ = INVALID
+
+-- Check GameState to see if we have a winner
+checkForWinner :: GameState -> WinState
+checkForWinner  _ = NONE
