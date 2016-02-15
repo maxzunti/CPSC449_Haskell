ff2c240ca67f95faa4d1d2919e6aa02802de6a60
Steven Vi <stevenvi@outlook.com>
2016-02-14 21:43:22 -0700

    Added reprompt to the input. Should only return an ints between 0-4. ApocStategyHuman should be finished now

--- ApocStrategyHuman.hs
+++ ApocStrategyHuman.hs
@@ -28,61 +28,95 @@ import Data.Maybe (fromJust, isNothing)
 import System.IO.Unsafe
 import ApocTools
 
-{- | This is just a placeholder for the human strategy: it always chooses to play
-     (0,0) to (2,1).
--}
 -- Need to check move legality here
-
+-- Gets input from user and return
 human    :: Chooser
 human b Normal        c = do
-      x <- getInput Normal
+      x <- getInput Normal c
       return (Just x)
-
 human b PawnPlacement c = do
-  x <- getInput PawnPlacement
+  x <- getInput PawnPlacement c
   return (Just x)
 
+-- make char into String
+charToString :: Char -> String
+charToString c = [c]
 
-getInput :: PlayType -> IO[(Int, Int)]
-getInput p = do
-  putStrLn "Enter the move coordinates for player ___ in the form"
+-- Get string of player for print
+playerToStr :: Player -> String
+playerToStr p | p == White = "White"
+              | p == Black = "Black"
+              | otherwise = "notValidPlayer"
+
+--gets the input of the player
+getInput :: PlayType -> Player -> IO[(Int, Int)]
+getInput pt pl = do
+  if (pt == Normal)
+  then putStrLn ("Enter the move coordinates for player " ++ (playerToStr pl) ++ " in the form 'srcX srcY destX destY' [0 >= n >= 4, or just enter return for a 'pass'] " ++ (charToString ((playerToStr pl) !! 0)) ++ "2:")
+  else putStrLn ("Enter the coordinates to place the pawn for player " ++ (playerToStr pl) ++ " in the form 'destX destY': [0 >= n >= 4] " ++ (charToString ((playerToStr pl) !! 0)) ++ "1:")
   input <- getLine
-  pos <- stringToList input p
-  return pos
+  if (checkEnoughVar (words input) pt && checkVarIsNum (words input) pt)
+  then do pos <- convertMoveType (words input) pt
+          return pos
+  else do
+    putStrLn "You have entered an invalid input! Try again" 
+    getInput pt pl
 
+--Check that their is Enough varibles for the move
+checkEnoughVar :: [String] -> PlayType -> Bool
+checkEnoughVar [] pt = False
+checkEnoughVar (x:xs) pt | length (x:xs) < 4 && (pt == Normal) = False
+                      | length (x:xs) < 2 && (pt == PawnPlacement) = False
+                      | otherwise = True
 
-stringToList :: String -> PlayType -> IO[(Int, Int)]
-stringToList x p = if (p == Normal)
-                  then if (length (words x) /= 4)
-                        then do return [(20,20),(20,20)]
-                        else do return [(readNumbers x !! 0,readNumbers x !! 1),(readNumbers x !! 2,readNumbers x !! 3)]
-                  else if (length (words x) /= 2)
-                        then do return [(20,20)]
-                        else do return [(readNumbers x !! 0, readNumbers x !! 1)]
+--Checks that the varibles can be converted into a integer
+--Checks if the integer is in the bound of the board
+checkVarIsNum :: [String] -> PlayType -> Bool
+checkVarIsNum x pt = if (pt == Normal)
+  then if checkStrsIsInt (fst (splitAt 4 x) )
+      then checkIntsBound (readStrNumbers (fst (splitAt 4 x) ))
+      else False
+  else if checkStrsIsInt (fst (splitAt 2 x) )
+      then checkIntsBound (readStrNumbers (fst (splitAt 2 x) ))
+      else False
 
---takes a string
-readNumbers :: String -> [Int]
-readNumbers s = if (checkInHum (words s))
-                then map toInt (words s)
-                else [20, 20, 20, 20]
+--turn String into a integer
+readStrNumbers :: [String] -> [Int]
+readStrNumbers s = map toInt s
 
-checkInHum :: [String] -> Bool
-checkInHum [] = False
-checkInHum (x:xs) | length (x:xs) == 4 && checkStrsIsInt (x:xs) = True
-                  | length (x:xs) == 2 && checkStrsIsInt (x:xs) = True
-                  | otherwise = False
+--check if a list of int is in the bound of the board
+checkIntsBound :: [Int] -> Bool
+checkIntsBound [] = True
+checkIntsBound (x:xs) = if (checkIntBound x)
+                      then checkIntsBound xs
+                      else False
 
+--check if a int is in the bound of the board
+checkIntBound :: Int -> Bool
+checkIntBound x = if (x >= 0 && x <= 4)
+  then True
+  else False
+
+--Turn a list of strings into the type of a move
+convertMoveType :: [String] -> PlayType -> IO[(Int, Int)]
+convertMoveType x p = if (p == Normal)
+                  then do return [(toInt (x !! 0), toInt (x !! 1)),(toInt (x !! 2), toInt (x !! 3))]
+                  else do return [(toInt (x !! 0), toInt (x !! 1))]
+
+--Checks if one String contains on numbers
 checkStrIsInt :: String -> Bool
 checkStrIsInt []     = True
 checkStrIsInt (x:xs) = if (isNumber x)
                       then checkStrIsInt xs
                       else False
 
+--Check if an entire String conntains only numbers
 checkStrsIsInt :: [String] -> Bool
 checkStrsIsInt []     = True
 checkStrsIsInt (x:xs) = if (checkStrIsInt x)
                       then checkStrsIsInt xs
                       else False
 
+--Turns a String into an Int
 toInt :: String -> Int
 toInt x = read x :: Int
